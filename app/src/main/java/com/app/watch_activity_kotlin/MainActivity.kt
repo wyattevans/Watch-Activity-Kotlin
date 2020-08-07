99fa4f3a39cda74bb2a2012b98affb3dc63a48bf
Wyatt Evans <syattevans@gmail.com>
2020-08-07 09:23:21 -0600

    Added fullBackupContent attribute.

--- app/src/main/java/com/app/watch_activity_kotlin/MainActivity.kt
+++ app/src/main/java/com/app/watch_activity_kotlin/MainActivity.kt
@@ -8,8 +8,6 @@ class MainActivity : WearableActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Enables Always-on
         setAmbientEnabled()
     }
 }
